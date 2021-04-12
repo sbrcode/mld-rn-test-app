b2b48602360835d1b1103571ea9bb6b0ec7e3de0
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2021-04-12 12:09:24 +0200

    display-playlist

--- src/pages/1/Page1.js
+++ src/pages/1/Page1.js
@@ -1,24 +1,46 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
 import { Button } from 'react-native-paper';
 import UserContext from '../../contexts/UserContext';
 import RoutesNames from '../../navigation/RoutesNames';
 import Alignments from '../../theme/Alignments';
 import Fonts from '../../theme/Fonts';
+import Api from '../../api/api';
+import { useEffect, useState } from 'react';
 
 export default function Page1({ navigation }) {
   const { logout } = useContext(UserContext);
 
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    Api.get()
+      .then((json) => setData(json.playlists.items))
+  }, []);
+
   return (
     <View style={[Alignments.fillColMain]}>
+      <FlatList
+        data={data}
+        keyExtractor={({ id }) => id}
+        numColumns={ 2 }
+        renderItem={({ item }) => (
+          <Image 
+            // mode={'contained'}
+            // onPress={() => navigation.navigate(RoutesNames.Page4)}
+            source={{ uri: item.images[0].url }}
+            style={{ width:179, height:179 }}
+          />
+        )}
+      />
       <Text style={Fonts.title}>Page1</Text>
-      <Button
+      {/* <Button
         mode={'contained'}
         onPress={() => navigation.navigate(RoutesNames.Page4)}
         style={styles.btn}
       >
         {'page 4 ->'}
-      </Button>
+      </Button> */}
 
       <Button mode={'contained'} onPress={logout} style={styles.btn} dark>
         {'logout'}
